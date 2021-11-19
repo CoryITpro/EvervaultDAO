0c6d0bcedc0719e367e885270cdfb3d48ee7dbb9
jeus0430 <jeus0430.tazan@gmail.com>
2021-11-20 03:29:53 +0900

    made routing work well

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import App from "./App";
 import Landing from "./Landing";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { loadTokenPrices } from "../helpers";
 import Loading from "../components/Loader";
 
 function Root() {
     const isApp = (): boolean => {
-        return window.location.host.includes("app");
+        return window.location.pathname != "/";
     };
 
     const [loading, setLoading] = useState(true);
@@ -19,9 +19,9 @@ function Root() {
     if (loading) return <Loading />;
 
     const app = () => (
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     );
 
     return isApp() ? app() : <Landing />;
