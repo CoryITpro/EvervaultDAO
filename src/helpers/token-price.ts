590f1b14d0667dec979b591789cb782326a769aa
jeus0430 <jeus0430.tazan@gmail.com>
2021-12-22 22:56:39 +0300

    usdt -> busd

--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -5,7 +5,6 @@ const cache: { [key: string]: number } = {};
 export const loadTokenPrices = async () => {
     const url = "https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2,olympus,magic-internet-money,tether,safeshiba,busd&vs_currencies=usd";
     const { data } = await axios.get(url);
-    cache["USDT"] = data["tether"].usd;
     cache["SAFESHIB"] = data["safeshiba"].usd;
     cache["BUSD"] = data["busd"].usd;
     cache["EVE"] = 0.0000000001;
